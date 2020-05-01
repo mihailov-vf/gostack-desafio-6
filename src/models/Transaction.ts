2f3c9eb74d7b0d647d7b674ec6d95ca99710eb90
Mihailov Vasilievic Filho <mihailov.vf@gmail.com>
2020-05-01 20:18:09 -0300

    Configurações para typeorm e migrations

--- src/models/Transaction.ts
+++ src/models/Transaction.ts
@@ -1,16 +1,40 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  ManyToOne,
+  JoinColumn,
+} from 'typeorm';
+import Category from './Category';
+
+@Entity('transactions')
 class Transaction {
+  @Column()
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column()
   title: string;
 
+  @Column({ enum: ['income', 'outcome'] })
   type: 'income' | 'outcome';
 
+  @Column()
   value: number;
 
+  @Column()
   category_id: string;
 
+  @ManyToOne(() => Category, { cascade: true })
+  @JoinColumn({ name: 'category_id' })
+  category: Category;
+
+  @CreateDateColumn()
   created_at: Date;
 
+  @UpdateDateColumn()
   updated_at: Date;
 }
 
